3bbc27d80dca076c508dd2923f6216f427991e74
n_nick <nasty.nickitina2011@yandex.ru>
2026-05-03 22:21:15 +0300

    change server

--- app.py
+++ app.py
@@ -7,19 +7,28 @@ import requests
 
 
 proc = subprocess.Popen(
-    [sys.executable, "-m", "uvicorn", "main:app"],
+    [sys.executable, "-m", "uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8000"],
     cwd="backend"
 )
 
 def wait_for_server():
-    for _ in range(20):
+    for _ in range(30):
         try:
-            # requests.get("http://127.0.0.1:8000/ping")
-            requests.get("http://85.239.49.27:8000/ping")
+            requests.get("http://127.0.0.1:8000/ping", timeout=1)
             return True
-        except:
-            time.sleep(0.5)
+        except requests.RequestException:
+            time.sleep(1)
     return False
+
+# def wait_for_server():
+#     for _ in range(20):
+#         try:
+#             # requests.get("http://127.0.0.1:8000/ping")
+#             requests.get("http://85.239.49.27:8000/ping")
+#             return True
+#         except:
+#             time.sleep(0.5)
+#     return False
 
 if wait_for_server():
     print("✅ Сервер запущен")
